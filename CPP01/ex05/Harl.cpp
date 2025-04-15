1ca172aed2d50b985fdf179e656d97e7c0fcb227
Alejandro <aleiestudio@gmail.com>
2025-04-15 08:56:15 +0000

    cpp01 completed

---
+++ CPP01/ex05/Harl.cpp
@@ -0,0 +1,39 @@
+#include "Harl.hpp"
+#include <iostream>
+
+void Harl::debug(void) {
+    std::cout << "[ DEBUG ]\n";
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\n";
+}
+
+void Harl::info(void) {
+    std::cout << "[ INFO ]\n";
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
+}
+
+void Harl::warning(void) {
+    std::cout << "[ WARNING ]\n";
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month.\n";
+}
+
+void Harl::error(void) {
+    std::cout << "[ ERROR ]\n";
+    std::cout << "This is unacceptable! I want to speak to the manager now.\n";
+}
+
+void Harl::complain(std::string level) {
+    std::string levels[] = { "DEBUG", "INFO", "WARNING", "ERROR" };
+    void (Harl::*actions[])() = {
+        &Harl::debug,
+        &Harl::info,
+        &Harl::warning,
+        &Harl::error
+    };
+
+    for (int i = 0; i < 4; i++) {
+        if (level == levels[i]) {
+            (this->*actions[i])();
+            return;
+        }
+    }
+}
